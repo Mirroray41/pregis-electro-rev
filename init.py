66e9b8b47cbc266735f63007bc09267cc5eb349b
mirroray41 <kockoviconline@gmail.com>
2025-05-20 10:09:03 +0200

    database insert

--- init.py
+++ init.py
@@ -1,6 +1,33 @@
 import streamlit as st
 import datetime
 from dateutil.relativedelta import relativedelta
+import sqlite3
+
+# DATABASE
+
+con = sqlite3.connect("revisions.db")
+cur = con.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS revisions(
+    device_name VARCHAR(5) NOT NULL,
+    service_date DATETIME NOT NULL,
+    tier INT NOT NULL,
+    project VARCHAR(45) NOT NULL,
+    building VARCHAR(45) NOT NULL,
+    state BOOLEAN NOT NULL,
+    technician VARCHAR(45) NOT NULL,
+    next_service DATETIME NULL,
+    location VARCHAR(45) NOT NULL,
+    ground_lead INT NULL,
+    isolation_resistance BOOLEAN NULL,
+    leakage_current INT NULL,
+    procesed BOOLEAN NULL,
+    PRIMARY KEY (device_name, service_date)
+)
+""")
+
+# STREAMLIT
 
 st.title('Import')
 
@@ -8,8 +35,20 @@ tier = st.segmented_control(
     "Třída", ("I", "II"), default="I", key=0
 )
 with st.form("import"):
-    technician = st.text_input("Jméno technika")
-    device_name = st.text_input("Identifikace spotřebiče")
+    technician = st.text_input("Jméno technika", placeholder="Př: NOVAKPE")
+    device_name = st.text_input("Identifikace spotřebiče", placeholder="X0123")
+    service_date = st.date_input("Datum servisu", datetime.datetime.now(), format="DD.MM.YYYY")
+    service_time = st.time_input("Čas servisu", datetime.datetime.now())
+
+    service_datetime = datetime.datetime(service_date.year, service_date.month, service_date.day, service_time.hour, service_time.minute)
+
+    project = st.text_input("Projekt", "REVIZE")
+    building = st.text_input("Budova", "PCL")
+    location = st.text_input("Umístění", "pcl")
+
+    state = st.segmented_control(
+        "Stav", ("Vyhovuje", "Nevyhovuje"), default="Vyhovuje", key=2
+    )
 
     isolation_resistance = st.segmented_control(
         "Izolační odpor", ("<200MOhm", "\>200MOhm"), default="\>200MOhm"
@@ -22,6 +61,34 @@ with st.form("import"):
 
     submitted = st.form_submit_button("Vložit")
 
+    if submitted:
+        if (technician and device_name):
+            command = f"""
+INSERT INTO revisions 
+    VALUES (
+        '{device_name}',
+        '{service_datetime.strftime("%Y-%m-%d-%H-%M")}',
+        {1 if tier == "I" else 2},
+        '{project}',
+        '{building}',
+        {int(state == "Vyhovuje")},
+        '{technician}',
+        '{service_date + relativedelta(years=2)}',
+        '{location}',
+        {ground_lead_current},
+        {int(isolation_resistance == "\>200MOhm")},
+        {leakage_current},
+        {int(False)}
+    );"""
+            cur.execute(command)
+            con.commit()
+        else:
+            if not technician:
+                st.error('Pole: Jméno technika, je prázdné')
+            if not device_name:
+                st.error('Pole: Identifikace spotřebiče, je prázdné')
+    
+
 with st.sidebar:
     st.title('Export')
 
@@ -29,7 +96,7 @@ with st.sidebar:
         tier = st.segmented_control(
             "Třída", ("I", "II"), default="I", key=1
         )
-        technician = st.text_input("Jméno technika")
+        technician = st.text_input("Jméno technika", placeholder="Nechte prázdné pro export všech")
         
         date_range = st.date_input(
             "Časový rozsah",
