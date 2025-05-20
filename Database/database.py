b65652fbdd5210da4091be8a2c12e7790bfe53ae
LowLightCZ1 <hajny.matej8@gmail.com>
2025-05-20 08:06:39 +0200

    Values added

--- Database/database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("revisions.db")
-cur = con.cursor()
-
-cur.execute("CREATE TABLE revisions(device_name)")
-
-res = cur.execute("SELECT name FROM sqlite_master")
-res.fetchone()
